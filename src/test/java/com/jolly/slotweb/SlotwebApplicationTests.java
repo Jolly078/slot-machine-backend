9b1697123520c79d4b654444538f17d2b112b98d
Jolly078 <oyololajosiah@gmail.com>
2025-05-15 22:36:52 +0100

    Upload backend files for slot machine

---
+++ src/test/java/com/jolly/slotweb/SlotwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jolly.slotweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlotwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
